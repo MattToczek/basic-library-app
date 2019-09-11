38a5081f3253d53f58661249a9d3c61d50e746d3
Matt. Toczek <toczek.matt@gmail.com>
2019-09-11 07:40:55 +0100

    adding checkOut() to customer and updating relevant properties

--- js/customers.js
+++ js/customers.js
@@ -5,4 +5,13 @@ class Customer {
     this.currentBook = null;
 
   }
+
+  checkOut(Book){
+      let twoWeeks = new Date(Date.now()+12096e5);
+      this.currentBook = Book;
+      Book.out = true;
+      Book.customer = customer.name;
+      Book.dueDate = twoWeeks;
+  }
+
 }
